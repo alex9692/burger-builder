b68b4c266d2021ef7b33c3fdabb5589bdd3261e4
alex kerketta <battle253@gmail.com>
2019-04-20 20:42:53 +0530

    added backdrop and button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Auxillary from "../../../hoc/Auxillary";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = props => {
 	const ingredientSummary = Object.keys(props.ingredients).map(
@@ -18,7 +19,16 @@ const orderSummary = props => {
 			<h3>Your Orders:</h3>
 			<p>A delicious burger with following ingredients:</p>
 			<ul>{ingredientSummary}</ul>
+			<p>
+				<strong>Total Price: {props.totalPrice.toFixed(2)}</strong>
+			</p>
 			<p>Continue to checkout?</p>
+			<Button btnType="Danger" clicked={props.purchaseCancelled}>
+				CANCEL
+			</Button>
+			<Button btnType="Success" clicked={props.purchaseContinued}>
+				CONTINUE
+			</Button>
 		</Auxillary>
 	);
 };
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,24 @@
 import React from "react";
 
 import classes from "./Modal.css";
+import Auxillary from "../.././../hoc/Auxillary";
+import Backdrop from "../Backdrop/Backdrop";
 
 const modal = props => {
 	const modalStatus = props.show ? classes.Show : classes.Hide;
 	return (
-		<div
-			className={classes.Modal + " " + modalStatus}
-			// style={{
-			// 	transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-			// 	opacity: props.show ? "1" : "0"
-			// }}
-		>
-			{props.children}
-		</div>
+		<Auxillary>
+			<Backdrop show={props.show} closeBackdrop={props.close} />
+			<div
+				className={classes.Modal + " " + modalStatus}
+				// style={{
+				// 	transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+				// 	opacity: props.show ? "1" : "0"
+				// }}
+			>
+				{props.children}
+			</div>
+		</Auxillary>
 	);
 };
 
